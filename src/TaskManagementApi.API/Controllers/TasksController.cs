0761b2a1b84490fc4da242c081eb460dd43c8290
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-11-27 14:19:18 -0600

    En API, se crea el controller de Tasks, la cadena de conexión y se registran los servicios en Program.cs

---
+++ src/TaskManagementApi.API/Controllers/TasksController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementApi.Application.Services;
+using TaskManagementApi.Core.Entities.TaskManagementApi.Core.Entities;
+
+namespace TaskManagementApi.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly TaskService _taskService;
+
+        public TasksController(TaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTask([FromBody] TaskItem task)
+        {
+            await _taskService.CreateTaskAsync(task);
+            return CreatedAtAction(nameof(GetTaskById), new { id = task.Id }, task);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTasks()
+        {
+            var tasks = await _taskService.GetAllTasksAsync();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTaskById(Guid id)
+        {
+            var task = await _taskService.GetTaskByIdAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return Ok(task);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(Guid id, [FromBody] TaskItem task)
+        {
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            await _taskService.UpdateTaskAsync(task);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(Guid id)
+        {
+            await _taskService.DeleteTaskAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("report")]
+        public async Task<IActionResult> GetTasksReport()
+        {
+            var report = await _taskService.GenerateReportAsync();
+            return Ok(report);
+        }
+    }
+}
--- src/TaskManagementApi.API/Program.cs
+++ src/TaskManagementApi.API/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagementApi.Application.Services;
+using TaskManagementApi.Core.Interfaces;
+using TaskManagementApi.Infrastructure.Data;
+using TaskManagementApi.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<TaskContext>(options => options.UseSqlite(connectionString));
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<TaskService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
