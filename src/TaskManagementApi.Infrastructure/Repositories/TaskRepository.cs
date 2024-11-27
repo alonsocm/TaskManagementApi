3d0a631d6a86860cbb7f2ee45027fb5ad11f509f
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-11-27 14:02:37 -0600

    Se implementa la interfaz del repositorio.

---
+++ src/TaskManagementApi.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementApi.Core.Entities.TaskManagementApi.Core.Entities;
+using TaskManagementApi.Core.Interfaces;
+using TaskManagementApi.Infrastructure.Data;
+
+namespace TaskManagementApi.Infrastructure.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly TaskContext _context;
+
+        public TaskRepository(TaskContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TaskItem> GetByIdAsync(Guid id)
+        {
+            return await _context.TaskItems.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
+        {
+            return await _context.TaskItems.ToListAsync();
+        }
+
+        public async Task AddAsync(TaskItem task)
+        {
+            _context.TaskItems.Add(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TaskItem task)
+        {
+            _context.TaskItems.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var task = await _context.TaskItems.FindAsync(id);
+            if (task != null)
+            {
+                _context.TaskItems.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
+
