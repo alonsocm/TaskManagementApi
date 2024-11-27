ac076669435e68ebf63cc806039931fcd417ca8c
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-11-27 14:01:59 -0600

    En el proyecto Application, se agrega el DTO que se utiliza para el reporte. Se agrega TaskService.

---
+++ src/TaskManagementApi.Application/DTOs/ReportDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagementApi.Application.DTOs
+{
+    public class ReportDto
+    {
+        public int TotalTasks { get; set; }
+        public int CompletedTasks { get; set; }
+        public int PendingTasks { get; set; }
+        public double CompletionRate { get; set; }
+    }
+}
